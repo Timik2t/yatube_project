9edc6ce7132feb014cca4770aa18dac01178c989
Timik2t <timik-95@yandex.ru>
2022-03-15 03:17:36 +0300

    img and CSS

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,8 +3,24 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    # Строку, которую надо вывести на страницу, тоже сохраним в переменную
+    title = 'Это главная страница проекта Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse('Страница группа постов')
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
