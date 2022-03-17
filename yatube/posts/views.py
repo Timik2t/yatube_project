93b1043cf0b70107af90b1208b9b126f592c1fb6
Timik2t <timik-95@yandex.ru>
2022-03-18 01:29:03 +0300

    pre finall

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-    template = 'posts/index.html'
-    # Строку, которую надо вывести на страницу, тоже сохраним в переменную
-    title = 'Это главная страница проекта Yatube'
-    # Словарь с данными принято называть context
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию
+    # (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        # В словарь можно передать переменную
-        'title': title,
+        'posts': posts,
     }
-    # Третьим параметром передаём словарь context
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
